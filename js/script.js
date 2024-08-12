9e735f625f5777a4acb4a19a81f0e7e4483748ca
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-08-11 19:36:06 -0700

    Finished the website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(function(){
-    $("#summer").click(function(){
-        $(".help").slideDown('slow');
-        $("#summer").fadeOut('slow');
-    });
+    $('.starter-image, .header, .overview, .gallery, .events, .contact-page').hide().fadeIn(1000);
+
+    $('.header, .starter-image, .container').hover(
+        function() {
+            // Mouse enters
+            $(this).css('transform', 'scale(1.1)');
+        },
+        function() {
+            // Mouse leaves
+            $(this).css('transform', 'scale(1)');
+        }
+    );
 });
