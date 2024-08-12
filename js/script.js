65ac9232f369fabd24da1411b0882ef9162743d6
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-08-11 19:42:15 -0700

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function(){
+    // Makes the content in each page fade in when you load in
     $('.starter-image, .header, .overview, .gallery, .events, .contact-page').hide().fadeIn(1000);
 
+    // Makes certain elements enlarge when you hover over them
     $('.header, .starter-image, .container').hover(
         function() {
             // Mouse enters
